5f2128945771a45dcc8f1fa2fb5a4da1f3f2829b
Shaun Robinson <connecshaun@gmail.com>
2021-10-20 20:47:36 +0000

    Create head function

---
+++ head.js
@@ -0,0 +1,22 @@
+//ASSERT EQUAL FUNCTION
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😁 😁 😁 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡 😡 😡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// HEAD FUNCTION TO DETERMINE "HEAD OF AN ARRAY"
+
+const head = function(array) {
+  if (array.length === 0) {
+    return undefined;
+  }
+  return array[0];
+};
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Chocolate", "Almonds", "Coconut"]), "Chocolate");
+

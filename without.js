2af421402acc6e29817e19f34798fb93c5e26327
Shaun Robinson <connecshaun@gmail.com>
2021-10-23 19:05:06 +0000

    Function to remove elements from a source array that are not present in an itemsToRemove array

---
+++ without.js
@@ -0,0 +1,55 @@
+const eqArrays = function (array, array2) {
+  if (array.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(eqArrays([1, 2, 3, 4], [1, 2, 3])) // false
+console.log(eqArrays([1, 2, 3, 4], [1, 3, 3, 4])) // false
+console.log(eqArrays([1, 3, 3], [1, 3, 3])) // true
+
+
+const assertArraysEqual = function (array, array2) {
+  if (!eqArrays(array, array2)) {
+    return console.log(`😡 😡 😡 Assertion Failed:, ${array} !== ${array2}`);
+  }
+
+  console.log(`😁 😁 😁 Assertion Passed:, ${array} === ${array2}`);
+
+};
+
+assertArraysEqual([1, 2, 3, 4], [1, 2, 3]) // false
+assertArraysEqual([5, 7, 9], [5, 9, 7]) // false
+assertArraysEqual([3, 2, 3], [3, 2, 3]) // true
+
+
+const without = function (source, itemsToRemove) {
+  let newArray = [];
+  for (let i = 0; i < source.length; i++) {
+    if (source[i] !== itemsToRemove[i]) {
+      newArray.push(source[i])
+    }
+  }
+
+  return newArray;
+};
+
+
+console.log(without([1, 2, 3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
+console.log(without(["hello", "world", "lighthouse"], ["lighthouse"])) // => ["hello", "world"]
+const words = ["hello", "world", "lighthouse"];
+console.log(without(words, ["lighthouse"]));
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+
+
+
+

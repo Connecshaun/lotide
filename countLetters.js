9b17732714db66a9a60e60c2f6c405ca0a7420b8
Shaun Robinson <connecshaun@gmail.com>
2021-10-26 03:24:48 +0000

    Create countLetters function that shows the number count for each of the letters in a string

---
+++ countLetters.js
@@ -0,0 +1,29 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`😁 😁 😁 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡 😡 😡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+const countLetters = function (string) {
+  let result = {}
+  for (const letters of string) {
+    if (letters !== " " && letters !== ",") {
+      if (result[letters]) {
+        result[letters] += 1
+      } else {
+        result[letters] = 1
+      }
+    }
+  }
+  return result
+}
+
+console.log(countLetters("Hello, jello"))
+const result1 = countLetters("Hello, Jello")
+assertEqual(result1["H"], 1);
+assertEqual(result1["e"], 2);
+

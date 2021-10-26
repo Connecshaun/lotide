0ad3e768ec527687ce1889329dabad544323fc9a
Shaun Robinson <connecshaun@gmail.com>
2021-10-26 22:18:49 +0000

    Create findKeyByValue function which will return the key that is associated to a specific value

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`😁 😁 😁 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡 😡 😡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+
+const findKeyByValue = function (object, value) {
+  for (const props in object) {
+    if (object[props] === value) {
+      return props
+    }
+  }
+
+}
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);

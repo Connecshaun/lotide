1e0c73f85e7285008a8d808424aa46d0267a5b12
Shaun Robinson <connecshaun@gmail.com>
2021-11-08 23:42:50 +0000

    Add require eqArrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,13 +1,10 @@
-const assertArraysEqual = function (array, array2) {
+const eqArrays = require(`./eqArrays`);
+
+const assertArraysEqual = function(array, array2) {
   if (!eqArrays(array, array2)) {
     return console.log(`😡 😡 😡 Assertion Failed:, ${array} !== ${array2}`);
   }
-
   console.log(`😁 😁 😁 Assertion Passed:, ${array} === ${array2}`);
-
 };
 
-assertArraysEqual([1, 4, 5], [1, 4, 5])
-assertArraysEqual([1, 3, 5], [1, 2, 7])
-
-
+module.exports = assertArraysEqual;

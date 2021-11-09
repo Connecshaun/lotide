378e73344b11c0f4c511cacf5753774164b2ee37
Shaun Robinson <connecshaun@gmail.com>
2021-11-09 06:15:27 +0000

    Complete test cases for middle

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,19 +1,20 @@
 const assert = require('chai').assert;
 const middle = require('../middle');
-// const assertArraysEqual = require('../assertArraysEqual');
-
 
 describe("#middle", () => {
   it("should return []", () => {
-    assert.deepEqual(middle([1]), [])
-  })
-})
-// console.log(middle(([1])))
-// console.log(middle(([1, 2])))
-// console.log(middle(([1, 7, 4, 2])))
-// console.log(middle(([1, 2, 3, 4, 5, 6])))
-// console.log(middle(([1, 2, 3, 4, 5, 6, 7])))
-// console.log(middle(([1, 3, 7, 5, 8, 6, 7])))
-// console.log(middle(["hello", "jello", "even"]))
-// assertArraysEqual(middle(([1, 2, 3, 4, 5, 6, 7])), [1, 3, 7, 5, 8, 6, 7])
-
+    assert.deepEqual(middle([1]), []);
+  });
+  it("should return []", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("should return [7. 4]", () => {
+    assert.deepEqual(middle([1, 7, 4, 2]), [7, 4]);
+  });
+  it("should return [4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
+  });
+  it("should return 'yellow'", () => {
+    assert.deepEqual(middle(["hello", "yellow", "mellow"]), ["yellow"]);
+  });
+});

731507d068fd1edd54614883b5a0e8b672f73bce
Shaun Robinson <connecshaun@gmail.com>
2021-11-09 01:42:42 +0000

    Test code updated using Chai assertions

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,15 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
+// const assertEqual = require('../assertEqual'); // now that we have chai's assertion library!! WOOOHOOOOOOOO!!!! 
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Chocolate", "Almonds", "Coconut"]), "Chocolate");
+describe("#head", () => {
+  it ("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+  it ("return '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5')
+  });
+  it ("returns 'Chocolate' for ['Chocolate', 'Almonds', 'Coconut']", () => {
+    assert.strictEqual(head(["Chocolate", "Almonds", "Coconut"]), 'Chocolate')
+  })
+})

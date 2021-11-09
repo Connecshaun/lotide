0f1aeb5d44a157da2997b0de975d8335ed7fa3c2
Shaun Robinson <connecshaun@gmail.com>
2021-11-09 05:50:46 +0000

    Update test cases to use mocha/chai and eslint

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,20 @@
-const assertEqual = require('../assertEqual');
+const expect = require('chai').expect;
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const result = tail(["Hello", "Lighthouse", "Labs", "Coffee", "Chocolate"]); //initial array test
-const result2 = tail([1, 2, 3, 4]); //another array test
-
-assertEqual(result.length, 4); // ensure we are seeing 4 elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-assertEqual(result[2], "Coffee"); // ensure third element is "Coffee"
-assertEqual(result[3], "Chocolate"); // ensure fourth element is "Labs"
-assertEqual(result2[0], 2); // ensure first element is 2
+describe("#tail", () => {
+  const result = tail(["Hello", "Lighthouse", "Labs", "Coffee", "Chocolate"]);
+  const result2 = tail([0, 2, 3, 4]);
+  it("Should only return last 4 of 5 elements", () => {
+    expect(result).to.eql(["Lighthouse", "Labs", "Coffee", "Chocolate"]);
+  });
+  it("The first element in the result array should be 'Lighthouse'", () => {
+    assert.equal(result[0], "Lighthouse");
+  });
+  it("The second element in the results array should be 'Labs'", () => {
+    assert.equal(result[1], "Labs");
+  });
+  it("The first element in the array shold be the number 2", () => {
+    assert.equal(result2[0], 2);
+  });
+});

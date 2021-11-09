457149f38aa10d152d071215c377c8d63edda83f
Shaun Robinson <connecshaun@gmail.com>
2021-11-09 05:36:00 +0000

    Eslint code

--- test/headTest.js
+++ test/headTest.js
@@ -1,15 +1,14 @@
 const assert = require('chai').assert;
 const head = require('../head');
-// const assertEqual = require('../assertEqual'); // now that we have chai's assertion library!! WOOOHOOOOOOOO!!!! 
 
 describe("#head", () => {
-  it ("returns 5 for [5, 6, 7]", () => {
+  it("returns 5 for [5, 6, 7]", () => {
     assert.strictEqual(head([5, 6, 7]), 5);
   });
-  it ("return '5' for ['5']", () => {
-    assert.strictEqual(head(['5']), '5')
+  it("return '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
   });
-  it ("returns 'Chocolate' for ['Chocolate', 'Almonds', 'Coconut']", () => {
-    assert.strictEqual(head(["Chocolate", "Almonds", "Coconut"]), 'Chocolate')
-  })
-})
+  it("returns 'Chocolate' for ['Chocolate', 'Almonds', 'Coconut']", () => {
+    assert.strictEqual(head(["Chocolate", "Almonds", "Coconut"]), 'Chocolate');
+  });
+});

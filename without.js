0bd23830afc260242d835c6f93939e2114c606ff
Shaun Robinson <connecshaun@gmail.com>
2021-11-10 01:43:43 +0000

    Add module.exports and eslint code

--- without.js
+++ without.js
@@ -1,55 +1,20 @@
-const eqArrays = function (array, array2) {
-  if (array.length !== array2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-console.log(eqArrays([1, 2, 3, 4], [1, 2, 3])) // false
-console.log(eqArrays([1, 2, 3, 4], [1, 3, 3, 4])) // false
-console.log(eqArrays([1, 3, 3], [1, 3, 3])) // true
-
-
-const assertArraysEqual = function (array, array2) {
-  if (!eqArrays(array, array2)) {
-    return console.log(`😡 😡 😡 Assertion Failed:, ${array} !== ${array2}`);
-  }
-
-  console.log(`😁 😁 😁 Assertion Passed:, ${array} === ${array2}`);
-
-};
-
-assertArraysEqual([1, 2, 3, 4], [1, 2, 3]) // false
-assertArraysEqual([5, 7, 9], [5, 9, 7]) // false
-assertArraysEqual([3, 2, 3], [3, 2, 3]) // true
-
-
-
-
-const without = function (source, itemsToRemove) {
+const without = function(source, itemsToRemove) {
   let newArray = [];
   for (let i = 0; i < source.length; i++) {
     if (!itemsToRemove.includes(source[i])) {
-      newArray.push(source[i])
+      newArray.push(source[i]);
     }
   }
-
   return newArray;
 };
 
+module.exports = without;
 
-
-console.log(without([1, 2, 3], [1])) // => [2, 3]
-console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
-const words = ["hello", "world", "lighthouse"];
-console.log(without(words, ["lighthouse"])); // => ["hello", "world"]
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+// console.log(without([1, 2, 3], [1])) // => [2, 3]
+// console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
+// const words = ["hello", "world", "lighthouse"];
+// console.log(without(words, ["lighthouse"])); // => ["hello", "world"]
+// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
 
 
 

3c29d6c65ba9869140f3144e7507c8c2cf3c83e9
Shaun Robinson <connecshaun@gmail.com>
2021-11-10 00:31:51 +0000

    Clean-up code with eslint

--- countLetters.js
+++ countLetters.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`😁 😁 😁 Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -6,24 +6,23 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-
-
-const countLetters = function (string) {
-  let result = {}
+const countLetters = function(string) {
+  let result = {};
   for (const letters of string) {
     if (letters !== " " && letters !== ",") {
       if (result[letters]) {
-        result[letters] += 1
+        result[letters] += 1;
       } else {
-        result[letters] = 1
+        result[letters] = 1;
       }
     }
   }
-  return result
-}
+  return result;
+};
 
-console.log(countLetters("Hello, jello"))
-const result1 = countLetters("Hello, Jello")
-assertEqual(result1["H"], 1);
-assertEqual(result1["e"], 2);
+// console.log(countLetters("Hello, jello"))
+// const result1 = countLetters("Hello, Jello")
+// assertEqual(result1["H"], 1);
+// assertEqual(result1["e"], 2);
 
+module.exports = countLetters;

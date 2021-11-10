b16be14bfdd4b4873d2b813230588d505cc72aad
Shaun Robinson <connecshaun@gmail.com>
2021-11-10 01:35:54 +0000

    Add module.exports and eslint code

--- map.js
+++ map.js
@@ -1,50 +1,10 @@
-const eqArrays = function (array, array2) {
-  if (array.length !== array2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
-const assertArraysEqual = function (array, array2) {
-  if (!eqArrays(array, array2)) {
-    return console.log(`😡 😡 😡 Assertion Failed:, ${array} !== ${array2}`);
-  }
-
-  console.log(`😁 😁 😁 Assertion Passed:, ${array} === ${array2}`);
-
-};
-
-
-
-const words = ["ground", "control", "to", "major", "tom"];
-
-const map = function (array, callback) {
+const map = function(array, callback) {
   const results = [];
   for (let item of array) {
-    results.push(callback(item))
+    results.push(callback(item));
   }
   return results;
-}
+};
 
-const result1 = map(words, word => word[0]);
-console.log(result1)
+module.exports = map;
 
-
-
-const callBack1 = function (item) {
-  return item + "-1"
-}
-
-const actual = map(words, callBack1)
-const expected = ['ground-2', 'control-1', 'to-1', 'major-1', 'tom-1'];
-
-
-
-assertArraysEqual(actual, expected);

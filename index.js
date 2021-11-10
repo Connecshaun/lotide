033ccf20d50a87f58d68973d8da2e5c6c0e729fc
Shaun Robinson <connecshaun@gmail.com>
2021-11-10 01:45:57 +0000

    Finish adding remaining functions to library and eslint code

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const eqObjects = require('./eqObjects');
 const findKey = require('./findKey');
 const findKeyByValue = require('./findKeyByValue');
 const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
 
 module.exports = {
   head: head,
@@ -26,4 +30,8 @@ module.exports = {
   findKey: findKey,
   findKeyByValue: findKeyByValue,
   flatten: flatten,
-}
+  letterPositions: letterPositions,
+  map: map,
+  takeUntil: takeUntil,
+  without: without
+};

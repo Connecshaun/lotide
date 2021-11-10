849b56a965e4768da30783e79316b543bf52b0ad
Shaun Robinson <connecshaun@gmail.com>
2021-11-10 01:01:36 +0000

    Add require statement and eslint code

--- eqObjects.js
+++ eqObjects.js
@@ -1,29 +1,6 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`😁 😁 😁 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😡 😡 😡 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const eqArrays = require('./eqArrays');
 
-const eqArrays = function (array, array2) {
-  if (array.length !== array2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
-
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
-const eqObjects = function (object1, object2) {
+const eqObjects = function(object1, object2) {
   let keys1 = Object.keys(object1);
   let keys2 = Object.keys(object2);
 
@@ -47,10 +24,4 @@ const eqObjects = function (object1, object2) {
   return true; //step 5
 };
 
-
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true); // => true
-
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false); // => false
+module.exports = eqObjects;
